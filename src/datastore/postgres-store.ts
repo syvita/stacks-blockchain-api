799ae86502c0e123ee78ffd8ebb63a961890eef7
MHassanTariq <mhtariq29@gmail.com>
2021-10-15 21:14:15 +0500

    fix: fixed error in api-tests

--- src/datastore/postgres-store.ts
+++ src/datastore/postgres-store.ts
@@ -4239,15 +4239,6 @@ export class PgDataStore
         tx.microblock_canonical,
       ]
     );
-
-    await client.query(
-      `
-      UPDATE txs
-      SET abi = $2
-      WHERE tx_id = $1
-      `,
-      [hexToBuffer(smartContract.tx_id), smartContract.abi]
-    );
   }
 
   async getSmartContract(contractId: string) {
